bd00dc2a4c002bf04ed0adbbe63b565149a8eee3
bpaumard <benjamin.paumard@viacesi.fr>
2019-07-16 10:23:27 +0200

    téléchargement : récupération du paramètre de la route

--- CISCO/src/Controller/DownloadFileFromEditorController.php
+++ CISCO/src/Controller/DownloadFileFromEditorController.php
@@ -5,16 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Service\DownloadFile;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class DownloadFileFromEditorController extends AbstractController
 {
     /**
-     * @Route("/download", name="download")
+     * @Route("/download/{fichier}", name="download")
      */
-    public function index(DownloadFile $downloadFile)
+    public function index(DownloadFile $downloadFile, $fichier)
     {
 
-        $response = $downloadFile->downloadFile('test', 'test123456789');
+        $response = $downloadFile->downloadFile('test', $fichier);
         return $response;
     }
 }
