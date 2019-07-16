01b0cb2af8adb59795a8cd0616992a1e9c7c1d7b
allan <all.brochard@gmail.com>
2019-07-16 15:37:29 +0200

    change DefaultController

--- CISCO/src/Controller/DefaultController.php
+++ CISCO/src/Controller/DefaultController.php
@@ -13,17 +13,25 @@ class DefaultController extends AbstractController
     public function index()
     {
         $liste_equipement = array();
-        for ($i = 1; $i < 255; $i++){
+        $minPing = 245;
+        for ($i = 255; $i > $minPing; $i--){
             $ip = '172.25.200.'.$i;
             $comu = 'cisco';
             $output = shell_exec('ping '.$ip.' -w 2 -c 1');
             if(strpos($output, ' 0%')==true){
                 $output = shell_exec('snmpwalk -v 2c -c '.$comu.' '.$ip.' .1.3.6.1.2.1.1.1.0');
                 if(strpos($output, 'Cisco')){
+                    $type = shell_exec('snmpwalk -v 2c -c '.$comu.' '.$ip.' .1.3.6.1.2.1.1.9.1.3.30');
+                    if(strpos($type, 'Switched')){
+                        $type='Switch';
+                    }else{
+                        $type='Rooter';
+                    }
                     array_push($liste_equipement, array(
                         'nom' => shell_exec('snmpwalk -v 2c -c '.$comu.' '.$ip.' .1.3.6.1.2.1.1.5.0'),
-                        'type' => shell_exec('snmpwalk -v 2c -c '.$comu.' '.$ip.' .1.3.6.1.2.1.1.5.0'),
-                    ));
+                        'type' => $type,
+                        'ip' => $ip,
+                     ));
                 }
             }
         }
@@ -31,6 +39,7 @@ class DefaultController extends AbstractController
         die;
         return $this->render('accueil.html.twig', [
             'controller_name' => 'DefaultController',
+            'liste_equipement' => $liste_equipement
         ]);
     }
     /**
