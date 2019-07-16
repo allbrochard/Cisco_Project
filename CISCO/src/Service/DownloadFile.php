94108c04246a41db391255f8f98d5ea867032377
bpaumard <benjamin.paumard@viacesi.fr>
2019-07-16 09:04:27 +0200

    correction date téléchargement

--- CISCO/src/Service/DownloadFile.php
+++ CISCO/src/Service/DownloadFile.php
@@ -5,33 +5,31 @@
  * Date: 15/07/2019
  * Time: 13:38
  */
-
 namespace App\Service;
 
-use Symfony\Component\Validator\Constraints\Date;
+//use Symfony\Component\Validator\Constraints\Date;
 
 class DownloadFile
 {
     public function downloadFile($titre, $contenue){
-
-        $date = (string) new Date('ddmmYY');
-        $filename = $titre.$date.'.txt';
-
+        $date = new \DateTime();
+        
+        $filename = $titre . $date->format('dmy') . '.txt';
+        
+        
         file_put_contents($filename, $contenue);
         // Generate response
         $response = new \Symfony\Component\HttpFoundation\Response();
-
         // Set headers
         $response->headers->set('Cache-Control', 'private');
         $response->headers->set('Content-type', mime_content_type($filename));
         $response->headers->set('Content-Disposition', 'attachment; filename="' . basename($filename) . '";');
         $response->headers->set('Content-length', filesize($filename));
-
         // Send headers before outputting anything
         $response->sendHeaders();
-
         $response->setContent(file_get_contents($filename));
-
+        dump($response);
+        die;
         return $response;
     }
 }
