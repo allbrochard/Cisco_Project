536a8db1d3fe681ee7d2ebfbe508744895f7be07
allan <all.brochard@gmail.com>
2019-07-17 10:00:00 +0200

    modif controller

--- CISCO/src/Controller/DefaultController.php
+++ CISCO/src/Controller/DefaultController.php
@@ -27,7 +27,7 @@ class DefaultController extends AbstractController
                 if(strpos($output, 'Cisco')){
                     $nom  = str_replace('"', '', shell_exec('snmpwalk -v 2c -c '.$comu.' '.$ip.' .1.3.6.1.2.1.1.5.0 -Ov -Oq'));;
                     $domaine = strstr($nom, '.');
-                    $nom = strstr($nom, '@', true);
+                    $nom = strstr($nom, '.', true);
                     dump($nom.'  '.$domaine);
                     if(strpos(shell_exec('snmpwalk -v 2c -c '.$comu.' '.$ip.' .1.3.6.1.2.1.1.9.1.3.30'), 'Switched')){
                         $type='Switch';
