c23f8a7382fae449c2e6985cf751e411b3caa37c
allan <all.brochard@gmail.com>
2019-07-17 16:47:15 +0200

    add service

---
+++ CISCO/src/Service/Fonction_equipement.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: allan
+ * Date: 15/07/2019
+ * Time: 13:38
+ */
+namespace App\Service;
+
+//use Symfony\Component\Validator\Constraints\Date;
+
+class Fonctionequipement
+{
+    public function indexFonction($action, $name){
+
+        if(isset($action) && function_exists($action)) {
+            $action = $action;
+            $var = isset($name) ? $name : null;
+            $getData = $action($var);
+            // do whatever with the result
+        }
+        switch($action) {
+            case('changer_nom'):
+                $response = $this->setEquipmentName($name);
+                break;
+            case 'get_conf':
+                $response = $this->getConf();
+                break;
+            default:
+                die('Access denied for this function!');
+        }
+        return $response;
+    }
+    function setEtatEquipement($bool){
+
+    }
+
+    function setEquipmentName($name){
+        $user = $_SESSION['user'];
+        $mpUser = $_SESSION['mp_user'];
+        $mpAdmin = $_SESSION['mp_admin'];
+        $ip = $_SESSION['ip_equipement'];
+
+        $response = shell_exec('/script/change_hostname '.$ip.' '.$user.' '.$mpUser.' '.$mpAdmin.' '.$name);
+        return $response;
+    }
+
+    function getConf(){
+        $conf = shell_exec('');
+        return $conf;
+    }
+}
