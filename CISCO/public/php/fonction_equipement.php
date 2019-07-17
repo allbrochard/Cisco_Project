aa4a59a72baed0dc6a8875921c67c7428496b9b7
Yoann <yoannpj@gmail.com>
2019-07-17 16:33:48 +0200

    POST -> GET

--- CISCO/public/php/fonction_equipement.php
+++ CISCO/public/php/fonction_equipement.php
@@ -1,14 +1,14 @@
 <?php
 
-if(isset($_POST['action']) && function_exists($_POST['action'])) {
-    $action = $_POST['action'];
-    $var = isset($_POST['name']) ? $_POST['name'] : null;
+if(isset($_GET['action']) && function_exists($_GET['action'])) {
+    $action = $_GET['action'];
+    $var = isset($_GET['name']) ? $_GET['name'] : null;
     $getData = $action($var);
     // do whatever with the result
 }
 switch($action) {
     case 'changer_nom':
-        setEquipmentName($_POST['name']);
+        setEquipmentName($_GET['name']);
     default:
         die('Access denied for this function!');
 }
