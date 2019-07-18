91ed25e5735ffe1544be091381ec9babbe44c707
allan <all.brochard@gmail.com>
2019-07-18 16:41:33 +0200

    modif interface

--- CISCO/src/Controller/InterfaceController.php
+++ CISCO/src/Controller/InterfaceController.php
@@ -13,7 +13,7 @@ class InterfaceController extends AbstractController
     public function index($name)
     {
         $comu = 'cisco';
-        $name = str_replace('-', '/', $name);
+        $name = str_replace(' ', '',str_replace('-', '/', $name));
         dump('snmpwalk -c '.$comu.' -v 2c '.$_SESSION['ip_equipement'].' 1.3.6.1.2.1.2.2.1.2 | grep \''.$name.'"\'');
         $response = shell_exec('snmpwalk -c '.$comu.' -v 2c '.$_SESSION['ip_equipement'].' 1.3.6.1.2.1.2.2.1.2 | grep \''.$name.'"\'');
         $num = strstr(str_replace('iso.3.6.1.2.1.2.2.1.2.', '', $response), ' =', true);
