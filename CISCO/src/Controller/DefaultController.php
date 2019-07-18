5aaee8d213c12b96f490e941c86f33fe8536a4ed
Yoann <yoannpj@gmail.com>
2019-07-18 09:42:12 +0200

    Update DefaultController.php

--- CISCO/src/Controller/DefaultController.php
+++ CISCO/src/Controller/DefaultController.php
@@ -70,7 +70,7 @@ class DefaultController extends AbstractController
             $domaine = strstr($nom, '.');
             $nom = strstr($nom, '.', true);
         }
-        $interfacesNames = shell_exec('snmpwalk -v 2c -c '.$comu.' '.$ip.' 1.3.6.1.2.1.2.2.1.2');
+        $interfacesNames = shell_exec('snmpwalk -v 2c -c '.$comu.' '.$ip.' 1.3.6.1.2.1.2.2.1.2 -Ov -Oq');
         $interfacesStatusAdmin = shell_exec('snmpwalk -v 2c -c '.$comu.' '.$ip.' 1.3.6.1.2.1.2.2.1.7');
         $interfacesStatusLinks = shell_exec('snmpwalk -v 2c -c '.$comu.' '.$ip.' 1.3.6.1.2.1.2.2.1.8');
         $tab = Array(str_split($interfacesNames, strpos($interfacesNames, ":")));
