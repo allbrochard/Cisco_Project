487fa23bf446ee8804426c0555542288a39b64d2
allan <all.brochard@gmail.com>
2019-07-18 13:41:36 +0200

    modif controller

--- CISCO/src/Controller/DefaultController.php
+++ CISCO/src/Controller/DefaultController.php
@@ -81,6 +81,7 @@ class DefaultController extends AbstractController
         }else{
             $domaine='';
         }
+        
         $interfacesNames = shell_exec('snmpwalk -v 2c -c '.$comu.' '.$ip.' 1.3.6.1.2.1.2.2.1.2 -Ov');
         $interfacesStatusAdmin = shell_exec('snmpwalk -v 2c -c '.$comu.' '.$ip.' 1.3.6.1.2.1.2.2.1.7 -Ov');
         $interfacesStatusLinks = shell_exec('snmpwalk -v 2c -c '.$comu.' '.$ip.' 1.3.6.1.2.1.2.2.1.8 -Ov');
@@ -89,15 +90,18 @@ class DefaultController extends AbstractController
         $tabStatusLinks = Array(explode("INTEGER:", $interfacesStatusLinks));
         $tabFinal = array();
         $tabVlan = array();
+
         for($i = 1; $i < count($tabNames[0])-1 ; $i++){
-            if(strpos($tabNames[0][$i], 'Vlan')){
+            if(strpos($tabNames[0][$i], 'Vlan') && $_SESSION['type']=='Switch'){
                 $tabV = array(
                     "NomInterface" => $tabNames[0][$i],
                     "StatutAdmin" => $tabStatusAdmin[0][$i],
                     "StatutLink" => $tabStatusLinks[0][$i]
                 );
                 array_push($tabVlan, $tabV);
-            }else {
+            }elseif(strpos( $tabNames[0][$i], 'Null')){
+                
+            }else{
                 $tab = array(
                     "NomInterface" => $tabNames[0][$i],
                     "StatutAdmin" => $tabStatusAdmin[0][$i],
@@ -106,8 +110,7 @@ class DefaultController extends AbstractController
                 array_push($tabFinal, $tab);
             }
         }
-        
-        dump($tabFinal);
+
         $equipement = array(
             'nom' => $nom,
             'type' => $type,
