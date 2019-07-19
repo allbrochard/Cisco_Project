374af74e8edf13c9239684e786fa37c62bdbf19b
allan <all.brochard@gmail.com>
2019-07-19 11:43:21 +0200

    modif interface

--- CISCO/src/Controller/InterfaceController.php
+++ CISCO/src/Controller/InterfaceController.php
@@ -37,9 +37,7 @@ class InterfaceController extends AbstractController
                     $request->request->get('mask')
                 );
             }
-            dump($request->request->get('etat') );
-            die;
-            if($request->request->get('etat') == 1){
+            if($request->request->get('numEtat') == 1){
                 $fonction_equipement->activerInterface($name);
             }else{
                 $fonction_equipement->desactiverInterface($name);
